3ab66e1ecdc252952c972f0060a6389f76fba86e
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2025-10-01 23:26:23 +0530

    Added Position Encoding and tokenizer

--- src/utils/tokenizer.py
+++ src/utils/tokenizer.py
@@ -1,25 +1,3 @@
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
-from datasets import load_dataset
+import tiktoken
 
-def train_bpe_tokenizer():
-  print("Beginning Training tokenizer")
-
-  # Load Dataset Codeparrot
-  dataset = load_dataset("codeparrot/codeparrot-clean", split="train", streaming=True)
-
-  total_samples = 500000
-
-  def get_training_corpus():
-    batch_size = 1000
-    iterator = iter(dataset)
-
-    run = True
-    while run:
-      batch = [item['content'] for item in zip(range(batch_size), iterator)]
-      if not batch:
-        run = False
-        break
-      yield batch
+tokenizer = tiktoken.get_encoding("cl100k_base")
