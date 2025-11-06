c65a2bbb4cd0d306ac8f3f087257cd5c69c09d46
Aatti13 <aattreya.ks@gmail.com>
2025-11-06 12:42:37 +0530

    Completed Model Init functions for m0, m1, m2, m3 models

--- models/m3_large.py
+++ models/m3_large.py
@@ -2,13 +2,15 @@ import torch
 import torch.nn as nn
 from src.config.model_config import ModelConfig, get_model_config
 from src.utils.model_utils import print_model_summary
+from src.model.llama4_arch import LLaMaForCausalLM
+
 
 def create_large_model(vocab_size: int = 32000):
   config = get_model_config("large")
   config.vocab_size = vocab_size
-  # model = LLaMa4ForCausalLM(config)
-  # return model
+  model = LLaMaForCausalLM(config)
+  return model
 
 if __name__ == "__main__":
   model = create_large_model()
-  # print_model_summary(model, get_model_config("minimal"))
+  print_model_summary(model, get_model_config("large"))
