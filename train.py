6d70f66b77f5e26dc2926e736350a558413f0ee0
Aatti13 <aattreya.ks@gmail.com>
2025-11-06 12:44:15 +0530

    Added Main Training and Inference Scripts

---
+++ train.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+from src.config.model_config import ModelConfig, MODEL_CONFIGS
+from src.model.llama4_arch import LLaMaForCausalLM
+from src.training.trainer import Trainer
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
